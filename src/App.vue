7967e498c69234d163e2891267864c853364f1de
SabaMuladze <muladze66@bk.ru>
2024-02-11 14:52:24 +0400

    make header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
+  <Header></Header>
   <router-view></router-view>
 </template>
 
-<script></script>
+<script>
+import Header from "./components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style>
 @tailwind base;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Home</div>
+  <div>Header</div>
 </template>
 
 <script></script>
