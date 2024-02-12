10d44e53f4a980cb2a6565c446bb8f9cec10516a
SabaMuladze <muladze66@bk.ru>
2024-02-13 00:08:01 +0400

    header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,23 @@
 <template>
-  <header class="w-full h-[130px] p-6">
-    <div class="flex justify-between items-center">
-      <div class="text-white text-3xl font-bold">devjobs</div>
-      <div class="flex items-center gap-3 h-fit">
-        <img src="../../public/assets/desktop/icon-sun.svg" alt="" />
-        <toggle @value="toggleVal"></toggle>
-        <img src="../../public/assets/desktop/icon-moon.svg" alt="" />
+  <div :class="{ dark: dark }">
+    <header
+      class="w-full h-[130px] p-6 bg-white dark:bg-[#121721] md:h-[160px] md:pt-9"
+    >
+      <div class="flex justify-between items-center lg:w-[1070px] lg:mx-auto">
+        <div class="text-white text-3xl font-bold">devjobs</div>
+        <div class="flex items-center gap-3 h-fit">
+          <img src="../../public/assets/desktop/icon-sun.svg" alt="" />
+          <toggle @value="toggleVal"></toggle>
+          <img src="../../public/assets/desktop/icon-moon.svg" alt="" />
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
+  </div>
 </template>
 
 <script>
 import Toggle from "./Toggle.vue";
+import { mapGetters } from "vuex";
 export default {
   components: {
     Toggle,
@@ -25,11 +30,32 @@ export default {
       this.changeDarkValue();
     },
   },
+  computed: {
+    ...mapGetters(["darkMode"]),
+    dark() {
+      return this.darkMode;
+    },
+  },
 };
 </script>
 
 <style scoped>
 header {
   background-image: url("../../public/assets/mobile/bg-pattern-header.svg");
+}
+@media (min-width: 768px) {
+  header {
+    background-image: url("../../public/assets/tablet/bg-pattern-header.svg");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+}
+@media (min-width: 1024px) {
+  header {
+    background-image: url("../../public/assets/desktop/bg-pattern-header.svg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    max-height: 160px;
+  }
 }
 </style>
